9bc215308f19df2feeb66caa4160334e64d05949
F1728819 Carlo Conte <carlo.conte@bb.com.br>
2023-09-27 11:08:23 -0300

    Implementa Redis

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const app = express();
+const { createClient } = require('redis');
 
-app.get('/', (req, res) => {
-    res.send({
-        ok: 1
+const redisClient = createClient();
+
+const getAllProducts = async() => {
+    const time = Math.random() * 5000;
+    return new Promise((resolve) => {
+        setTimeout(() => {
+           resolve(["Produto 1","Produto 2"]) ;
+        }, time);
     });
+};
+
+app.get('/', async (req, res) => {
+    const productsFromCache = await redisClient.get('getAllProducts');
+    if(productsFromCache){
+        res.send(JSON.parse(productsFromCache));
+    };
+    const products = await getAllProducts();
+    await redisClient.set("getAllProducts", JSON.stringify(products));
+    res.send(products);
 });
 
-app.listen(3000, () => {
-    console.log('server running on port 3000...');
-});
+const startup = async() => {
+    await redisClient.connect();
+    app.listen(3000, '0.0.0.0', () => {
+        console.log('server running on port 3000...');
+    });
+};
+
+startup();
